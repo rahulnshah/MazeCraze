f594212bffded7fa47654ab461941edcceb92f9b
Rahul Shah <rahulnshah3012@gmail.com>
2024-07-06 18:27:37 -0400

    applied singleton pattern to Maze class

--- src/MazeServer.java
+++ src/MazeServer.java
@@ -161,6 +161,11 @@ public class MazeServer {
         }
     }
 
+    public void suggestClosestPath()
+    {
+
+    }
+
     public static void main(String [] args)
     {
         MazeServer server = new MazeServer();
@@ -172,7 +177,7 @@ public class MazeServer {
 
         // Instantiate a single shared Maze object in the server class.
         // This instance will be accessed and modified by all client threads.
-        maze = new Maze();
+        maze = Maze.getInstance();
         // initialize the maze
         maze.initialize();
 
